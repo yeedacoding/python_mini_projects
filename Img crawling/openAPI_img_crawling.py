5d350fb16cd37d2fd18361b2def3033d582e7d74
yeedacoding <taeheun0325@naver.com>
2021-07-26 23:59:59 +0900

    Create openAPI_img_crawling.py

---
+++ Img crawling/openAPI_img_crawling.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+
+def save_image(image_url, file_name):
+    img_response = requests.get(image_url)
+    # 요청에 성공했다면,
+    if img_response.status_code == 200:
+        # 파일 저장
+        with open(file_name, "wb") as fp:
+            fp.write(img_response.content)
+
+
+# 이미지 검색
+url = "https://dapi.kakao.com/v2/search/image"
+headers = {"Authorization": "KakaoAK <your rest api app key"}
+data = {"query": "펭수"}
+url.encode("UTF8")
+# 이미지 검색 요청
+response = requests.post(url, headers=headers, data=data)
+
+# 요청에 실패했다면,
+if response.status_code != 200:
+    print("error! because ", response.json())
+else:  # 성공했다면
+    count = 0
+    print(response.json())
+    for image_info in response.json()['documents']:
+        print(f"[{count}th] image_url = ", image_info['image_url'])
+        # 저장될 이미지 파일명 설정
+        count = count + 1
+        file_name = "test_%d.jpg" % (count)
+        # 이미지 저장
+        save_image(image_info['image_url'], file_name)
